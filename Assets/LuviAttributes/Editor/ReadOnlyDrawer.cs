2ac3b050e5422ad3ff512a166a7a9864c6e1f599
Thanut Panichyotai <luvikung@gmail.com>
2026-06-22 00:27:58 +0700

    Fix bug: no namespace on ReadOnlyAttribute and it's drawer

--- Assets/LuviAttributes/Editor/ReadOnlyDrawer.cs
+++ Assets/LuviAttributes/Editor/ReadOnlyDrawer.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
+namespace LuviKunG.Attributes.Editor
 {
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
     {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        bool cacheEnabled = GUI.enabled;
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = cacheEnabled;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            bool cacheEnabled = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = cacheEnabled;
+        }
     }
 }
