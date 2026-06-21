a1166edd543a99fc4dfdef604f45bf4323e24ee6
Thanut Panichyotai <luvikung@gmail.com>
2026-06-22 00:27:58 +0700

    Fix bug: no namespace on ReadOnlyAttribute and it's drawer

--- Scripts/ReadOnlyAttribute.cs
+++ Scripts/ReadOnlyAttribute.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using UnityEngine;
 
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-public class ReadOnlyAttribute : PropertyAttribute
+namespace LuviKunG.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
 
+    }
 }
